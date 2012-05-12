85ff96c0c3785ff43e670659d504c8d93094971a
David Yu <lewen.yu@gmail.com>
2012-05-12 08:34:53 -0200

    added brackets to ensure order of operation you can never be too sure!

--- src/cEntity.cpp
+++ src/cEntity.cpp
@@ -3,20 +3,20 @@
 cEntity::cEntity()
         :m_AnimFPS(10), m_AnimLooping(true)
 {
-    m_Anim.SetTicksPerFrame((int) (1000.0f * 1.0f / m_AnimFPS));
+    m_Anim.SetTicksPerFrame((int) (1000.0f * (1.0f / m_AnimFPS)));
 }
 
 cEntity::cEntity(Vec2f& pos, Vec2f& dim, Vec2f& vel)
         :m_Pos(pos), m_Dim(dim), m_Vel(vel), m_AnimLooping(true),
          m_AnimFPS(10)
 {
-    m_Anim.SetTicksPerFrame((int) (1000.0f * 1.0f / m_AnimFPS));
+    m_Anim.SetTicksPerFrame((int) (1000.0f * (1.0f / m_AnimFPS)));
 }
 
 cEntity::cEntity(float x, float y, float w, float h, float vx, float vy)
         :m_AnimFPS(10), m_AnimLooping(true)
 {
-    m_Anim.SetTicksPerFrame((int) (1000.0f * 1.0f / m_AnimFPS));
+    m_Anim.SetTicksPerFrame((int) (1000.0f * (1.0f / m_AnimFPS)));
     m_Pos = Vec2f(x, y);
     m_Dim = Vec2f(w, h);
     m_Vel = Vec2f(vx, vy);
